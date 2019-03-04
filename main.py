670d6b4bbab462c74a92ded6e25d31dd82eddfb8
kevin <kevin@onfarminnovations.com>
2019-03-04 10:39:46 -0500

    tweaks

--- main.py
+++ main.py
@@ -6,31 +6,32 @@ from jinja2 import Template
 from report import report
 from urlparse import urlparse
 
-import time
-import codecs
+import sys, time, codecs
 
-import pdb
+# import pdb
 
 #Pick a topic to generate a brief about
-term = "income inequality"
+search_term = str(sys.argv[1])
+print "\n\tSearch Term: {}\n".format(search_term)
 
 #Search Bing for links from reliable sources
 print "Searching for links to evaluate..."
-links = google(term,3)
+links = google(search_term, 3)
 
-#Get article text for each link
+# #Get article text for each link
 print "Getting summary..."
 articles = []
 for link in links:
     articles.append(algorithmia(link))
+    print link
 
 if articles:
     for article in articles:
-        print "  {}".format(article.url)
-        print "  {}".format(article.title)
-        print "  {}".format(article.summary.encode('utf-8'))
+        print " {}".format(article.url)
+        print " {}".format(article.title)
+        print " {}".format(article.summary.encode('utf-8'))
         print "\n\t----------------------\n"
 else:
-    print "Error, sort of: no articles found for '{}'?".format(term)
+    print "Error, sort of: no articles found for '{}'?".format(search_term)
 
 # pdb.set_trace()
